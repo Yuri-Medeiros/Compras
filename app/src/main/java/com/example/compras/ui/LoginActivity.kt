9a74ea277d8794ebf20153e07bbb866994e5f7c2
yuri-medeiros <yuri06medeiros@gmail.com>
2025-09-20 17:49:55 -0300

    Tela de login 80%

---
+++ app/src/main/java/com/example/compras/ui/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.compras.ui
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding =
+    }
+}
